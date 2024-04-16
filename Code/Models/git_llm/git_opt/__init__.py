3057e1c3f1dd9db53fbb917379096c06bdf24127
Kim Gun Il <lurker18@naver.com>
2024-04-16 20:14:37 +0900

    Add Git-OPT model

---
+++ Code/Models/git_llm/git_opt/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 16 20:00:24 2024
+
+@author: lurker18
+"""
+
+from Code.Models.git_llm.git_opt.modeling_GIT_OPT import GitOPTConfig, GitOPTForCausalLM, GitOPTModel
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export WANDB_PROJECT=GIT_Llama
-export PROJECT_NAME=llama/exp001
+export PROJECT_NAME=opt/exp001
 export WANDB_NAME=$PROJECT_NAME
 
 deepspeed train.py --config_file projects/$PROJECT_NAME.yml
