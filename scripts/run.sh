a09c27d1a02dc7666cf48058aeded6f064c206c5
DESKTOP-J8AELIN\Nova18 <lurker18@naver.com>
2024-05-23 00:22:07 +0900

    Add GitMPT model and misc settings

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export WANDB_PROJECT=GIT_Llama
-export PROJECT_NAME=opt/exp001
+export WANDB_PROJECT=GIT_MPT
+export PROJECT_NAME=mpt/exp001
 export WANDB_NAME=$PROJECT_NAME
 
 deepspeed train.py --config_file projects/$PROJECT_NAME.yml
